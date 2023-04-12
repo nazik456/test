f22882f1351836eede2e3631640d97d9e993fc3f
samagatova456 <samagatovanazik0@gmail.com>
2023-04-12 20:08:13 +0300

    opened a new file products

--- src/App.js
+++ src/App.js
@@ -3,46 +3,40 @@ import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
-import { createContext } from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import Delivery from "./pages/Delivery";
+import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore/lite";
-import { categoryCollection, productsCollection } from "./firebase";
+import { categoryCollection } from "./firebase";
 
+// Создать контекст, который будет хранить данные.
 export const AppContext = createContext({
   categories: [],
-
 });
 
 function App() {
   const [categories, setCategories] = useState([]);
-  const[products, setProducts] = useState([])
-  useEffect(() => {
-    getDocs(categoryCollection).then(({ docs }) => {
-      setCategories(
-        docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }))
-      );
-    });
+
+  useEffect(() => { // выполнить только однажды
+    getDocs(categoryCollection) // получить категории
+      .then(({ docs }) => { // когда категории загрузились
+        setCategories( // обновить состояние
+          docs.map(doc => ({ // новый массив
+            ...doc.data(), // из свойств name, slug
+            id: doc.id // и свойства id
+          }))
+        )
+      });
   }, []);
 
-  getDocs(productsCollection).then(({ docs }) => {
-    setProducts(
-      docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }))
-    )
-  });
-}, []);
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories ,products }}>
+      <AppContext.Provider value={{ categories }}>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/contacts" element={<h1>Contacts</h1>} />
+            <Route path="/delivery" element={<h1>Delivery</h1>} />
             <Route path="/categories/:slug" element={<Category />} />
 
             <Route path="*" element={<NotFound />} />
@@ -51,6 +45,6 @@ function App() {
       </AppContext.Provider>
     </div>
   );
-
+}
 
 export default App;
