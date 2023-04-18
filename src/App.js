6dda4330747b68decae46ac60e7fe77ad0e12dd5
samagatova456 <samagatovanazik0@gmail.com>
2023-04-18 14:31:45 +0300

    context for shopping cart

--- src/App.js
+++ src/App.js
@@ -9,17 +9,23 @@ import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore/lite";
 import { categoryCollection, productCollection } from "./firebase";
 import Contacts from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
 
 // Создать контекст, который будет хранить данные.
 export const AppContext = createContext({
   categories: [],
   products:[],
+// контекст для корзины
+cart:{},// содержимое корзинку
+setCart: () =>{}, //изменить 
 });
 
 
 function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] =useState([]);
+  const [ cart, setCart] = useState([]);
 
   useEffect(() => { // выполнить только однажды
     getDocs(categoryCollection) // получить категории
@@ -46,14 +52,16 @@ function App() {
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories , products}}>
+      <AppContext.Provider value={{ categories , products , cart, setCart }}>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/about" element={<About/>} />
             <Route path="/contacts" element={<Contacts/>} />
             <Route path="/delivery" element={<Delivery/>} />
             <Route path="/categories/:slug" element={<Category />} />
+            <Route path="/products/:slug" element={<Product/>} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
