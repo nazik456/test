bc9b3aa1e904c3be8fea910260e1a5b83722e3a8
samagatova456 <samagatovanazik0@gmail.com>
2023-05-01 15:57:21 +0300

    export order.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import About from "./pages/About";
-import Delivery from "./pages/Delivery";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
@@ -7,35 +5,39 @@ import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore/lite";
-import { categoryCollection, onAuthChange, productCollection } from "./firebase";
-import Contacts from "./pages/Contact";
+import { categoryCollection, onAuthChange, ordersCollection, productsCollection } from "./firebase";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
-import ThankYou from "./pages/Thankyou";
+import ThankYou from "./pages/ThankYou";
+import Orders from "./pages/Orders";
 
 // Создать контекст, который будет хранить данные.
 export const AppContext = createContext({
   categories: [],
-  products:[],
-// контекст для корзины
-cart:{},// содержимое корзинку
-setCart: () =>{}, //изменить 
+  products: [],
+  Orders: [],
+  // контекст для корзины
+  cart: {}, // содержимое корзинки
+  setCart: () => {}, // изменить содержимое корзики
 
-user:null,
+  user: null,
 });
-
 
 function App() {
   const [categories, setCategories] = useState([]);
-  const [products, setProducts] =useState([]);
+  const [products, setProducts] = useState([]);
+ const [orders,setOrders] = useState([]);
+
   const [cart, setCart] = useState(() => {
     return JSON.parse(localStorage.getItem('cart')) || {};
   });
 
-const[user,setUser] = useState([null]);
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cart));
   }, [cart]);
+
   useEffect(() => { // выполнить только однажды
     getDocs(categoryCollection) // получить категории
       .then(({ docs }) => { // когда категории загрузились
@@ -47,8 +49,7 @@ const[user,setUser] = useState([null]);
         )
       });
 
-
-    getDocs(productCollection) // получить категории
+    getDocs(productsCollection) // получить категории
       .then(({ docs }) => { // когда категории загрузились
         setProducts( // обновить состояние
           docs.map(doc => ({ // новый массив
@@ -57,26 +58,35 @@ const[user,setUser] = useState([null]);
           }))
         )
       });
+
+      getDocs(ordersCollection) // получить категории
+      .then(({ docs }) => { // когда категории загрузились
+        setOrders( // обновить состояние
+          docs.map(doc => ({ // новый массив
+            ...doc.data(), // из свойств name, slug
+            id: doc.id // и свойства id
+          }))
+        )
+      });
+    onAuthChange(user => {
+      setUser(user);
+    });
   }, []);
-  onAuthChange (user =>{
-    setUser(user);
-  })
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories , products , cart, setCart }}>
+      <AppContext.Provider value={{ categories, products, cart, setCart, user, orders }}>
         <Layout>
           <Routes>
-        
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/contacts" element={<Contacts/>} />
-            <Route path="/delivery" element={<Delivery/>} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/contacts" element={<h1>Contacts</h1>} />
+            <Route path="/delivery" element={<h1>Delivery</h1>} />
             <Route path="/categories/:slug" element={<Category />} />
-            <Route path="/products/:slug" element={<Product/>} />
-            <Route path="/thank-you" element={<ThankYou/>} />
-
+            <Route path="/products/:slug" element={<Product />} />
+            <Route path="/thank-you" element={<ThankYou />} />
+            <Route path="/orders" element={<Orders />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
---
+++ src/pages/Orders.js
@@ -0,0 +1,7 @@
+export default function Orders(){
+  return (
+    <div className="Orders">
+      <h1>Orders</h1>
+    </div>
+  )
+}
