7b1090b817fe2d4eb5a7272e9eb2680be6ff5391
samagatova456 <samagatovanazik0@gmail.com>
2023-05-02 21:42:22 +0300

    auth corrected

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,24 @@
-import { singIn } from "../../firebase"
+import { useContext } from "react";
+import { logIn, logOut } from "../../firebase";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
 
-export default function Auth(){
-  
+export default function Auth() {
+  const { user } = useContext(AppContext);
+
   return (
     <div className="Auth">
-      <button onClick={singIn}>Sing in</button>
+      {user ? (
+        <span>
+          Hello <Link to="/orders">{user.displayName}</Link>!
+          <button onClick={logOut}>Sign out</button>
+        </span>
+      ) : (
+        <span>
+          Hello guest!
+          <button onClick={logIn}>Sign in</button>
+        </span>
+      )}
     </div>
-  )
+  );
 }
