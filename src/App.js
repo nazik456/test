fdaaf8f33c0785bc04d9df3dcf8b63abe90ce454
samagatova456 <samagatovanazik0@gmail.com>
2023-05-12 14:56:17 +0300

    import  delete About

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import { createContext, useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore";
+import About from "./pages/About";
+
 import {
   onAuthChange,
   onCategoriesLoad,
   onOrdersLoad,
   onProductsLoad,
-  ordersCollection,
-  productsCollection,
+  
 } from "./firebase";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
 import ThankYou from "./pages/ThankYou";
-import Orders from "./pages/Orders";
-import About from "./pages/About";
+
+
 
 // Создать контекст, который будет хранить данные.
 export const AppContext = createContext({
@@ -75,10 +76,11 @@ function App() {
             <Route path="/delivery" element={<h1>Delivery</h1>} />
             <Route path="/categories/:slug" element={<Category />} />
             <Route path="/products/:slug" element={<Product />} />
-            <Route path="/thank-you" element={<ThankYou />} />
-            <Route path="/orders" element={<Orders />} />
-
-            <Route path="*" element={<NotFound />} />
+            <Route path="/Thankyou" element={<ThankYou />} />
+            <Route path="/orders" element={<orders/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="*" element={<NotFound />} />  
+   
           </Routes>
         </Layout>
       </AppContext.Provider>
