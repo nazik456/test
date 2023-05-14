ae613b871c93df898a121e9a5359cc2ad23e496b
samagatova456 <samagatovanazik0@gmail.com>
2023-05-14 16:49:16 +0300

    nav changed

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,30 +1,26 @@
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
 
-
 export default function Nav() {
   return (
     <nav className="Nav">
       <div className="NavMonth">
-        </div>
-  
-      <ul className="ul">
-        <li className="NavItem">
-          
-          < NavLink to="/about">About</NavLink>
-    
-        </li>
-        <li className="NavItem">
-          <NavLink to="/Home">Home</NavLink>
-        </li>
-        
-        <li className="NavItem">
-          <NavLink to="/contacts">Contacts</NavLink>
-        </li>
-        <li className="NavItem">
-          <NavLink to="/delivery">About Company</NavLink>
-        </li>
-      </ul>
+        <ul className="ul">
+          <li className="NavItem">
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li className="NavItem">
+            <NavLink to="/Home">Home</NavLink>
+          </li>
+
+          <li className="NavItem">
+            <NavLink to="/contacts">Contacts</NavLink>
+          </li>
+          <li className="NavItem">
+            <NavLink to="/delivery">About Company</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
