7bb9d8b87ecb55898618aa53eb9c62282487fbc0
samagatova456 <samagatovanazik0@gmail.com>
2023-05-15 15:06:29 +0300

    productList css corrected codes and changed

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -6,27 +6,46 @@ import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/Addproduct";
 import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
-export default function ProductList({ category}) {
+export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-  const output = products.filter(product => product.category === category.id)
-  .map((product) =>(
-    <div  key={product.id} className="Product">
-      <img src={product.picture} alt={product.name} />
-      <NavLink to={'/products/' + product.slug}>
-        {product.name}
-      </NavLink>
-      <span>{product.price} </span>
-      <AddToCart product={product} />
-      <DeleteProduct product={product} /> 
-    </div>
-  ));
+  const output = products
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div key={product.id} className="Products">
+        <div className="div-inside-product">
+          <div className="picture-wrap">
+            <img
+              className="picture picture--main"
+              loading="eager"
+              width="300px"
+              src={product.picture}
+              alt={product.name}   
+            />
+            <img
+              className="picture picture--hover"
+              loading="eager"
+              width="300px"
+              src={product.picture2}
+              alt={product.name}
+            />
+          </div>
+          <div className="Pro">
+            <NavLink className="nav" to={"/products/" + product.slug}>
+              {product.name}
+            </NavLink>
+            <span className="price-in-product">{product.price} som</span>
+            <AddToCart product={product} />
+            <DeleteProduct product={product} />
+          </div>
+        </div>
+      </div>
+    ));
 
   return (
     <div className="ProductList">
       {output}
       <AddProduct category={category} />
-
     </div>
   );
 }
