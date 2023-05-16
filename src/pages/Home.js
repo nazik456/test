d49fbe44a293a566f8a063a3bfd2a5b9e1395c9f
samagatova456 <samagatovanazik0@gmail.com>
2023-05-16 15:51:27 +0300

    put a photo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ export default function Home(){
     <div className="Home">
       <h1>Home</h1>
       <p>hello</p>
+      <img src="https://biz.com.kg/wp-content/uploads/2020/10/WhatsApp-Image-2020-09-30-at-13.06.46.jpeg" ></img>
+      
+      <img src="https://igotmoney.ru/wp-content/uploads/2018/08/topkanc.jpeg"></img>
     </div>
   )
 }
