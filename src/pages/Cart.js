cec40d309d5ca542d1e1fd58b2c8cdefd156c2a7
samagatova456 <samagatovanazik0@gmail.com>
2023-05-17 15:02:26 +0300

    import cart css

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,6 @@
 import CartList from "../components/CartList/CartList";
 import Orderform from "../components/OrderForm/OrderForm";
-
+import "./Cart.css";
 export default function Cart(){
   return(
   <div className="Cart">
