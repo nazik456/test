5be5d9701be8d31ecbab22251d7fbff931108fde
samagatova456 <samagatovanazik0@gmail.com>
2023-05-17 21:42:13 +0300

    imported and put a photo

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,11 @@
+import "./Contact.css";
 export default function Contact(){
   return(
     <div className="Contact">
-      <h1>Home</h1>
-      <p>hello</p>
+<h4>Contact</h4>
+<h5>+997 505 500 273</h5>
+    <img src="https://w7.pngwing.com/pngs/83/881/png-transparent-telephone-call-computer-icons-iphone-call-screening-outgoing-angle-electronics-telephone-call-thumbnail.png"></img>
+  
     </div>
   )
 }
