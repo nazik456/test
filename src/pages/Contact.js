7089af318824378242165c5453271f001052c554
samagatova456 <samagatovanazik0@gmail.com>
2023-05-17 23:01:07 +0300

    delete img leave your address and number

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,12 @@ export default function Contact(){
   return(
     <div className="Contact">
 <h4>Contact</h4>
+<h1>phone:</h1>
 <h5>+997 505 500 273</h5>
-    <img src="https://w7.pngwing.com/pngs/83/881/png-transparent-telephone-call-computer-icons-iphone-call-screening-outgoing-angle-electronics-telephone-call-thumbnail.png"></img>
+<h1>Adress:</h1>
+<h5>carlamax 53</h5>
+   
+
   
     </div>
   )
