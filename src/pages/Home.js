a0fbf485184ba9a206fd7b3f4e9968f2e7ec0b69
samagatova456 <samagatovanazik0@gmail.com>
2023-05-17 15:03:05 +0300

    changed the text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
-export default function Home(){
-  return(
-    <div className="Home">
-      <h1>Home</h1>
-      <p>hello</p>
-      <img src="https://biz.com.kg/wp-content/uploads/2020/10/WhatsApp-Image-2020-09-30-at-13.06.46.jpeg" ></img>
-      
-      <img src="https://igotmoney.ru/wp-content/uploads/2018/08/topkanc.jpeg"></img>
-    </div>
-  )
+import "./Home.css";
+export default function Home() {
+return (
+<div className="Home">
+<h1>Address</h1>
+<img src="https://biz.com.kg/wp-content/uploads/2020/10/WhatsApp-Image-2020-09-30-at-13.06.46.jpeg" alt="image1" />
+<img src="https://igotmoney.ru/wp-content/uploads/2018/08/topkanc.jpeg" alt="image2" />
+<img src="https://moybiznes.org/wp-content/uploads/magazin-kanctovarov-1024x624.jpg" alt="image3" />
+</div>
+);
 }
+
+
+
+
