0c400eb7bee13b79aa5062b300307bd1e93ac331
samagatova456 <samagatovanazik0@gmail.com>
2023-05-18 15:17:48 +0300

    Delete img

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -2,17 +2,17 @@
 export default function Delivery(){
   return(
     <div className="Delivery">
-      <h1>Fast Delivery</h1>
+      <h1>Fast Delivery:</h1>
     <p>Our delivery services are fast and reliable.
-      We make sure that your package arrives at its destination in the shortest time possible</p>
+      We make sure that your package arrives at its destination in the shortest time possible.</p>
   
       <hr/>
-      <h1>Secure Delivery</h1>
-      <p> Your packages are safe with us .We take extra meacures to ensure that your packges are delivered securely and without any damage</p>
+      <h1>Secure Delivery:</h1>
+      <p> Your packages are safe with us .We take extra meacures to ensure that your packges are delivered securely and without any damage.</p>
       <hr/>
-      <h1>Flexible Delivery OPtions</h1>
-      <p>We offer variety of delivery options to sult your needs .Whether you package delivered on a specitic date on time we have you covered</p>
-      <img src="https://tile3.maps.2gis.com/tiles?x=46350&y=24113&z=16&v=1&ts=online_hd"></img>
+      <h1>Flexible Delivery OPtions:</h1>
+      <p>We offer variety of delivery options to sult your needs .Whether you package delivered on a specitic date on time we have you covered.</p>
+
     </div>
   )
 }
