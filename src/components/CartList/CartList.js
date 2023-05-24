b1f309dc0e469e5efc37bd65510d2fae6593c9ad
samagatova456 <samagatovanazik0@gmail.com>
2023-05-24 14:57:03 +0300

    style has changed

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -1,40 +1,50 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AppContext } from "../../App";
-import "./CartList.css";
+  import { useContext } from "react";
+  import { Link } from "react-router-dom";
+  import { AppContext } from "../../App";
+  import "./CartList.css";
 
-export default function CartList() {
-  const { products, cart, setCart } = useContext(AppContext);
+  export default function CartList() {
+    // получить список товаров и корзину
+    const { products, cart, setCart } = useContext(AppContext);
 
-  function onQuantityChange(product, gty){
-    setCart({
-      ...cart,
-      [product.id]: gty
-    })
+    function onQuantityChange(product, qty) {
+      setCart({
+        ...cart,
+        [product.id]: qty,
+      });
+    }
+
+    function onItemRemove(product) {
+      const newCart = { ...cart };
+      delete newCart[product.id];
+      setCart(newCart);
+    }
+
+    // получить только ID товаров в корзинке
+    const productIds = Object.keys(cart);
+
+    // чтобы вывести пройтись по товаром
+    const output = products
+      // удалить все товары которые не в корзине
+      .filter((product) => productIds.includes(product.id))
+      // вывести товары и их количество
+      .map((product) => (
+        <div className="CartItem" key={product.id}>
+          <img src={product.picture} alt={product.name} />
+          <div className="Card-content">
+            <Link to={"/product/" + product.slug}>{product.name}</Link>
+            {cart[product.id]}
+            <input
+              type="number"
+              value={cart[product.id]}
+              min={1}
+              onChange={(event) => onQuantityChange(product, +event.target.value)}
+            />
+            <span>{cart[product.id] * product.price} ₽</span>
+            <button onClick={() => onItemRemove(product)}>Remove</button>
+          </div>
+        </div>
+      ));
+
+    return <div className="CartList">{output}</div>;
   }
- function onItemRemove(product){
-  const newCart = {...cart};
-  delete newCart[product.id];
-  setCart(newCart);
- }
-  const productIds = Object.keys(cart);
-
-  const output = products
-  .filter((product) => productIds.includes(product.id))
-  .map((product) => (
-    <div className="CartItem" key={product.id}>
-      <img src={product.picture} alt={product.name} />
-      <Link to={"/product/" + product.slug}>{product.name}</Link>
-      <input type="number" value={cart[product.id]} min={1} onChange={(event)=>onQuantityChange(product, +event.target.value)}  />
-     
-      <span>{cart[product.id] * product.price} </span>
-      <button onClick={() => onItemRemove(product)}>Remove</button>
-    </div>
-  ));
-
-  return (
-    <div className="CartList">
-      {output}
-    </div>
-  )
-}
