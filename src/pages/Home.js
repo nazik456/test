cb352f8f4f680e4ee7da5f7d75ab16e201b2c394
samagatova456 <samagatovanazik0@gmail.com>
2023-05-27 19:34:46 +0300

    codes changed and sizes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,30 @@
-  import "./Home.css";
-  export default function Home() {
+import React from 'react';
+import './Home.css';
+
+export default function Home() {
   return (
-  <div className="Home">
- 
-    <p>
-              Address:{" "}
-              <a href="https://goo.gl/maps/VeQdviXxBjLxcnMh6">
-                {" "}
-                st. Pushkina, 10, Moscow
-              </a>
-            </p>
-   
-  
-  <img src="https://biz.com.kg/wp-content/uploads/2020/10/WhatsApp-Image-2020-09-30-at-13.06.46.jpeg" alt="image1" />
-  
-  <img src="https://igotmoney.ru/wp-content/uploads/2018/08/topkanc.jpeg" alt="image2" />
-  <img src="https://moybiznes.org/wp-content/uploads/magazin-kanctovarov-1024x624.jpg" alt="image3" />
- 
-  </div>
+    <div className="Home">
+      <p>
+        Address:{" "}
+        <a href="https://goo.gl/maps/VeQdviXxBjLxcnMh6">
+          st. Pushkina, 10, Moscow
+        </a>
+      </p>
+
+      <img
+        src="https://biz.com.kg/wp-content/uploads/2020/10/WhatsApp-Image-2020-09-30-at-13.06.46.jpeg"
+        alt="image1"
+      />
+
+      <img
+        src="https://igotmoney.ru/wp-content/uploads/2018/08/topkanc.jpeg"
+        alt="image2"
+      />
+
+      <img
+        src="https://moybiznes.org/wp-content/uploads/magazin-kanctovarov-1024x624.jpg"
+        alt="image3"
+      />
+    </div>
   );
-  }
-
-
-
-
+}
